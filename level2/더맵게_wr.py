05302af2fceaac18d71d9a8f18bde7ad4cd336a8
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-09-09 12:24:47 +0900

    Update 더맵게_wr.py

--- level2/더맵게_wr.py
+++ level2/더맵게_wr.py
@@ -1,18 +1,24 @@
-# 23.8점
+# 38.1점
 
 def solution(scoville, K):
     answer = 0
     scoville.sort()
-    for num in scoville:
-        if num < K:
-            while True:
-                scoville.sort()
-                mix = scoville[0] + scoville[1]*2
-                del scoville[0]
-                del scoville[0]
-                answer += 1
-                if mix < K:
-                    scoville.append(mix)
-                else:
-                    break
-    return answer
+    while True:
+        scoville.sort()
+        mix = scoville[0] + scoville[1]*2
+        del scoville[0]
+        del scoville[0]
+        answer += 1
+        if mix < K:
+            scoville.append(mix)
+        scoville.sort()
+        check = 0
+        for num in scoville:
+            if num >= K:
+                check += 1
+            else:
+                break
+        if len(scoville) == check:
+            return answer
+        elif len(scoville) == 1 and (scovile[0] < K):
+            return -1
